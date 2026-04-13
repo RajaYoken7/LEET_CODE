6483f910849aeb9e4419d129a40e38aaa708cce4
Raja Yoken <rajayoken072@gmail.com>
2026-04-13 12:07:27 +0530

    Create 15. 3Sum.py
    This 3Sum solution uses a Sorting and Two-Pointer strategy to find all unique triplets that sum to zero in  time. By first sorting the array, the algorithm fixes one element and "squeezes" two pointers (left and right) to find matches, efficiently skipping duplicate values to ensure the final result set contains only distinct triplets. This approach optimizes performance by reducing the search space and avoiding the complexity of a brute-force nested loop.

---
+++ 15. 3Sum.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def threeSum(self, nums):
+        new_arr = []
+        nums.sort()  # Sort to make skipping duplicates easy
+        
+        for i in range(len(nums) - 2):
+           
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+                
+            left = i + 1
+            right = len(nums) - 1
+            
+            
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+                
+                if curr_sum == 0:
+                    new_arr.append([nums[i], nums[left], nums[right]])
+                    
+                    
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    
+                    left += 1
+                    right -= 1
+                elif curr_sum < 0:
+                    left += 1
+                else:
+                    right -= 1
+                    
+        return new_arr
