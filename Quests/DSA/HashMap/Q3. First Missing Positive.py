d15cc616b32a336bc9ad2581d64ff188d32b6fa4
RajaYoken7 <rajayoken072@gmail.com>
2026-07-03 00:04:52 +0530

    solved quest problems in leetcode

---
+++ Quests/DSA/HashMap/Q3. First Missing Positive.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        nums.sort()
+
+        missing = 1
+
+        for num in nums:
+            if num < missing:
+                continue
+            elif num == missing:
+                missing += 1
+            else:
+                break
+
+        return missing
